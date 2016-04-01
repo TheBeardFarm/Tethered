dbe0b60da874aebae4ee75a2fb93e43f96ff8882
Steven Osowski <sosowski@comcast.net>
2016-03-31 21:06:44 -0400

    Fixing Code Formatting
    Laziness prevails

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,33 +12,29 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //ANIMATOR CODE IS FOR A 2D PROJECT - Uncomment if this is going to be a 2D game
-        //And attach an animator with a "direction" int parameter.
-        var leftKey = Input.GetKey("left");
-        var rightKey = Input.GetKey("right");
+        	//ANIMATOR CODE IS FOR A 2D PROJECT - Uncomment if this is going to be a 2D game
+        	//And attach an animator with a "direction" int parameter.
+        	var leftKey = Input.GetKey("left");
+        	var rightKey = Input.GetKey("right");
 
-        var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        var speed = 5.0f;
+        	var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        	var speed = 5.0f;
 
-        //If holding both keys, don't move
-        if (leftKey && rightKey)
-        {
-            //animator.SetInteger("Direction", 0);
-        }
-        else if (leftKey)
-        {
-            //animator.SetInteger("Direction", -1);
-            transform.position += move * speed * Time.deltaTime;
-        }
-        else if (rightKey)
-        {
-            //animator.SetInteger("Direction", 1);
-            transform.position += move * speed * Time.deltaTime;
-        }
-        //If no keys down, don't move
-        else
-        {
-            //animator.SetInteger("Direction", 0);
-        }
+        	//If holding both keys, don't move
+        	if (leftKey && rightKey) {
+        	    //animator.SetInteger("Direction", 0);
+        	}
+        	else if (leftKey) {
+         	    //animator.SetInteger("Direction", -1);
+         	    transform.position += move * speed * Time.deltaTime;
+       		}
+        	else if (rightKey) {
+                //animator.SetInteger("Direction", 1);
+           	    transform.position += move * speed * Time.deltaTime;
+        	}
+        	//If no keys down, don't move
+        	else {
+                //animator.SetInteger("Direction", 0);
+        	}
 	}
 }
