907a056433315f01b6a49c0871572e45031916e4
Steven Osowski <sosowski@comcast.net>
2016-04-02 17:28:17 -0400

    (hopefully) fixes tabbing, removes copy file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,90 +4,91 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-	private Animator _animator;
+    private Animator _animator;
     private Rigidbody2D _rb2d;
-	public float _horizontalSpeed = 5f;
+    public float _horizontalSpeed = 5f;
     private Transform groundCheck;
 
-	[SerializeField]
-	private PlayerIdentity _identity;
+    [SerializeField]
+    private PlayerIdentity _identity;
 
+    //Flags for jump control
     private bool grounded = false;
     public bool jump = false;
 
-	private void Start()
-	{
+    private void Start()
+    {
         _animator = this.GetComponent<Animator>();
         _rb2d = this.GetComponent<Rigidbody2D>();
         _rb2d.drag = 1;
         groundCheck = transform.Find("groundCheck");
-	}
+    }
 
-	private void Update()
-	{
-		HandleInput();
+    private void Update()
+    {
+        HandleInput();
         grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
         if (IsUpButtonDown() && grounded)
         {
             jump = true;
         }
-	}
+    }
 
     void FixedUpdate()
     {
-        if(jump)
+        if (jump)
         {
             _rb2d.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
             jump = false;
         }
     }
 
-	private void HandleInput()
-	{
-		bool leftKey = IsLeftButtonDown();
-		bool rightKey = IsRightButtonDown();
+    private void HandleInput()
+    {
+        bool leftKey = IsLeftButtonDown();
+        bool rightKey = IsRightButtonDown();
 
-		if (leftKey)
-		{
-			HandleInputLeft();
-		}
-		if (rightKey)
-		{
-			HandleInputRight();
-		}
+        if (leftKey)
+        {
+            HandleInputLeft();
+        }
+        if (rightKey)
+        {
+            HandleInputRight();
+        }
 
         if (!leftKey && !rightKey)
         {
             HandleInputNone();
         }
-	}
+    }
 
-	private bool IsLeftButtonDown()
-	{
-		switch (_identity)
-		{
-			case PlayerIdentity.Red:
-				return Input.GetKey("left");
+    private bool IsLeftButtonDown()
+    {
+        switch (_identity)
+        {
+            case PlayerIdentity.Red:
+                return Input.GetKey("left");
 
-			case PlayerIdentity.Blue:
-				return Input.GetKey("a");
-		}
-		throw new NotSupportedException();
-	}
+            case PlayerIdentity.Blue:
+                return Input.GetKey("a");
+        }
+        throw new NotSupportedException();
+    }
 
-	private bool IsRightButtonDown()
-	{
-		switch (_identity)
-		{
-			case PlayerIdentity.Red:
-				return Input.GetKey("right");
+    private bool IsRightButtonDown()
+    {
+        switch (_identity)
+        {
+            case PlayerIdentity.Red:
+                return Input.GetKey("right");
 
-			case PlayerIdentity.Blue:
-				return Input.GetKey("d");
-		}
-		throw new NotSupportedException();
-	}
+            case PlayerIdentity.Blue:
+                return Input.GetKey("d");
+        }
+        throw new NotSupportedException();
+    }
 
     private bool IsUpButtonDown()
     {
@@ -102,15 +103,15 @@ public class PlayerController : MonoBehaviour
         throw new NotSupportedException();
     }
 
-	private void HandleInputLeft()
-	{
+    private void HandleInputLeft()
+    {
         _rb2d.velocity = new Vector2(-_horizontalSpeed, _rb2d.velocity.y);
-	}
+    }
 
-	private void HandleInputRight()
-	{
+    private void HandleInputRight()
+    {
         _rb2d.velocity = new Vector2(_horizontalSpeed, _rb2d.velocity.y);
-	}
+    }
 
     private void HandleInputNone()
     {
