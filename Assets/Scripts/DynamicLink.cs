83000aeb723d2ca852cefed2e110da2d41f78957
Joe Van Deusen <daeleth@gmail.com>
2016-04-13 22:12:57 -0400

    Dynamic Link Progress
    Discovered an implemented one possible start to a mostly physics
    (non-Kinematic) solution to tether/link mechanic:
    
    Still experimenting with flaws/advantages and seeing what can be learned
    from this implementation. It may seem like a Kinematic solution is the
    most controlled way to go about this. Right now hold the down arrow key
    to keep the update function running. It is set like this for testing
    purposes, however it shows how a possible "stiff" tether might be
    implemented in the future.
    
    *Features in development/debugging:*
    - 2 player joined, and able to inflict forces onto each other
    - Link Dynamically changes size, effectively only limiting the max
    distance.
    - Link will be able to be switched between kinematic modes, so
    developers may experiment which is best.

--- Assets/Scripts/DynamicLink.cs
+++ Assets/Scripts/DynamicLink.cs
@@ -1,54 +1,91 @@
 using System;
 using UnityEngine;
 
+// TODO: figure out the proper components needed
 // [RequireComponent(typeof(BoxCollider2D))]
 // [RequireComponent(typeof(DistanceJoint2D))]
 public class DynamicLink : MonoBehaviour {
 
 	// object at the "start" of the link
-	[SerializeField]
-	public Rigidbody2D TargetA
+	public Rigidbody2D TargetStart
 	{
-		get { return targetA; }
+		get { return targetStart; }
 		set
 		{
-			targetA = value;
+			targetStart = value;
 			if (distanceLimiter) {
 				distanceLimiter.connectedBody = value;
 			}
-			LinkTargets();
+			if (connectorStart) {
+				connectorStart.connectedBody = value;
+			}
 		}
 	}
 
 	// object at the "end" of the link (this will attach to another thing, like a player, or nothing if not set)
-	[SerializeField]
-	public Rigidbody2D TargetB
+	public Rigidbody2D TargetEnd
 	{
-		get { return targetB; }
+		get { return targetEnd; }
 		set
 		{
-			targetB = value;
-
-			LinkTargets();
+			targetEnd = value;
+			if (connectorEnd) {
+				connectorEnd.connectedBody = value;
+			}
+		}
+	}
+	// Anchor point for the connection in the starting object's local space.
+	public Vector2 AnchorPointStart
+	{
+		get { return connectorStart ? connectorStart.connectedAnchor : Vector2.zero; }
+		set
+		{
+			if (distanceLimiter) {
+				distanceLimiter.connectedAnchor = value;
+			}
+			if (connectorStart) {
+				connectorStart.connectedAnchor = value;
+			}
+		}
+	}
+	// Anchor point for the connection in the ending object's local space.
+	public Vector2 AnchorPointEnd
+	{
+		get { return connectorEnd ? connectorEnd.connectedAnchor : distanceLimiter ? distanceLimiter.anchor : Vector2.zero; }
+		set
+		{
+			if (connectorEnd) {
+				connectorEnd.connectedAnchor = value;
+			}
+		}
+	}
+	// should reflect this rigid body's 
+	public bool IsKinematic
+	{
+		get { return isKinematic; }
+		set
+		{
+			if (thisRigidbody && thisRigidbody.isKinematic != value) {
+				thisRigidbody.isKinematic = value;
+			}
+			// disable the connector component if its kinematic, as it messes with things
+			if (value == true && connectorStart && connectorStart != false) {
+				connectorStart.enabled = false;
+			} else if (connectorStart.enabled != true) {
+				connectorStart.enabled = true;
+			}
+			isKinematic = value;
 		}
 	}
 
-	// Anchor point for the connection in object A's local space.
-	public Vector2 AnchorPointB
-	{
-		get { return anchorPointB; }
-		set
-		{
-			anchorPointB = value;
-		}
-	}
-
-	private Rigidbody2D targetA;
-	private Rigidbody2D targetB;
+	private Rigidbody2D thisRigidbody;
+	private Rigidbody2D targetStart;
+	private Rigidbody2D targetEnd;
 	private DistanceJoint2D distanceLimiter;
+	private DistanceJoint2D connectorStart;
+	private DistanceJoint2D connectorEnd;
 	private Collider2D hingeHitbox;
-	private Vector2 anchorPointB = Vector2.zero;
-	private Vector3 orgionalScale;
+	private bool isKinematic = false;
 	private float size;
 	// private float endLinkBuffer = 0.2f;
 
@@ -57,24 +94,32 @@ public class DynamicLink : MonoBehaviour {
 		// TODO: look into whether this is needed or not, and if it is better for GetComponent
 	}
 
-	// Use this for initialization
 	void Start () {
+		// get all the necessary components
+		DistanceJoint2D[] connectors = GetComponents<DistanceJoint2D>();
+		distanceLimiter = connectors[0];
+		connectorStart = connectors[1];
+		connectorEnd = connectors[2];
+		hingeHitbox = GetComponent<Collider2D>();
+		thisRigidbody = GetComponent<Rigidbody2D>();
+		IsKinematic = thisRigidbody.isKinematic;
+		// set the "size" of the object based on its anchor points. default full size is 1.
+		size = sizeFromAnchors();
+		// set the anchor points for the "start" of the link
+		targetStart = distanceLimiter.connectedBody;
+		connectorStart.connectedBody = distanceLimiter.connectedBody;
+		// set the anchor points for the "end" of the link
+		targetEnd = connectorEnd.connectedBody;
+		// last but not least, color it red.
 		GetComponent<Renderer>().material.color = Color.red;
-		distanceLimiter = GetComponent<DistanceJoint2D>();
-		hingeHitbox =  GetComponent<Collider2D>();
-		orgionalScale = transform.localScale;
-		size = (transform.TransformPoint(Vector3.up) - transform.position).magnitude;
-		// now set the anchor point
-		targetA = distanceLimiter.connectedBody;
 		LinkTargets();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		// UpdateLink();
 		if(Input.GetKey("down")) {
-			LinkTargets();
+			UpdateLink();
 		}
 	}
 
@@ -87,11 +132,29 @@ public class DynamicLink : MonoBehaviour {
 		
 		// instantiate a new DynamicLink object as a child of this one positioned at the hinge point.
 		
-		// set the targetA of the new object to this object
+		// set the targetStart of the new object to this object
 		
-		// set the new object's targetB to this object's targetB
+		// set the new object's targetEnd to this object's targetEnd
 		
-		// set this targetB to the new object.
+		// set this targetEnd to the new object.
+	}
+
+	private float sizeFromAnchors()
+	{
+		// default to a size of 1;
+		if (distanceLimiter.anchor == Vector2.zero && connectorStart.anchor == Vector2.zero) {
+			return 1f;
+		}
+		return (distanceLimiter.anchor - connectorStart.anchor).magnitude;
+	}
+
+	private Vector2 directionFromAnchors()
+	{
+		// default to up;
+		if (distanceLimiter.anchor == Vector2.zero && connectorStart.anchor == Vector2.zero) {
+			return Vector2.up;
+		}
+		return (distanceLimiter.anchor - connectorStart.anchor).normalized;
 	}
 
 	private void LinkTargets()
@@ -103,27 +166,33 @@ public class DynamicLink : MonoBehaviour {
 		Vector2 scaleVector;
 		float newScale;
 		
-		// TODO: debug everything.
-		// print("link started");
-		startPoint = targetB ? targetB.gameObject.transform.TransformPoint(anchorPointB) : transform.TransformPoint(distanceLimiter.anchor);
-		// transform.position = (targetB.transform.position + anchorPointB) - distanceLimiter.anchor;
-		if (targetA) {
-			endPoint = distanceLimiter.connectedBody.gameObject.transform.TransformPoint(distanceLimiter.connectedAnchor);
+		// TODO: debug everything ever.
+		if (targetStart) {
+			startPoint = distanceLimiter.connectedBody.gameObject.transform.TransformPoint(AnchorPointStart);
+			endPoint = targetEnd ? targetEnd.gameObject.transform.TransformPoint(AnchorPointEnd) : transform.TransformPoint(distanceLimiter.anchor);
 			distance = (startPoint - endPoint);
-			anchorDirection = distanceLimiter.anchor == Vector2.zero ? new Vector2(.5f,0f) : distanceLimiter.anchor.normalized;
+			anchorDirection = directionFromAnchors();
 			scaleVector = new Vector2(Mathf.Abs(anchorDirection.x), Mathf.Abs(anchorDirection.y));
-			// print(scaleVector);
-			// TODO: LEFT OFF HERE: might have fixed the following, but check: i think i have to round the size to a reasonable amount... the end scale isn't turning out correctly.
-			// TODO: Also fix scale vector... i think i just need logic if its 1, maybe.
-			// TODO: at this point we are assuming the object's size in global space 1, this could be improved.
 			newScale = distance.magnitude/size;
-			// print(newScale);
+			// TODO: incorperate a "width" or "thinkness" setting and modify scale based on this as well.
 			transform.localScale = new Vector3((scaleVector.x * newScale)+(1 - scaleVector.x), (scaleVector.y * newScale)+(1 - scaleVector.y), 1);
-			// set the position to the midpoint in the distance
-			transform.position = Vector3.Lerp(startPoint, endPoint, 0.5f);
-			transform.rotation = Quaternion.FromToRotation(anchorDirection, new Vector3(distance.normalized.x, distance.normalized.y, 0));
+			// in kinematic mode set the position to the midpoint in the distance manually
+			// if (isKinematic) {
+				transform.position = Vector3.Lerp(startPoint, endPoint, 0.5f);
+				transform.rotation = Quaternion.FromToRotation(-anchorDirection, new Vector3(distance.normalized.x, distance.normalized.y, 0));
+			// }
 			// TODO: test and adapt for sizes that would be different from 1.
 		}
+	}
+
+	private void UpdateLink()
+	{
+		// Check and Update variables used to connect the two links
+		// TODO: look into a better way to keep the kinematic value sysnced with the rigidbody component's value... pointers maybe? some kind of event when its changed, so I can disable components?
+		if (thisRigidbody && thisRigidbody.isKinematic != isKinematic) {
+			IsKinematic = thisRigidbody.isKinematic;
+		}
+		LinkTargets();
 	}
 }
 
